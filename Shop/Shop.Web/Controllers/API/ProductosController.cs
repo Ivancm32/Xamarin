08f0f36e497d3e3c3c887a00f33351c3a5462b1d
ivanmetalhammer6 <ivanmetalhammer6@gmail.com>
2020-04-29 11:32:15 -0600

    API Controlador de Productos

---
+++ Shop/Shop.Web/Controllers/API/ProductosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductosController : Controller
+    {
+        private readonly IRepositorioProductos  repositorioProductos;
+
+        public ProductosController(IRepositorioProductos repositorioProductos)
+        {
+            this.repositorioProductos = repositorioProductos;
+        }
+
+        [HttpGet]
+        public IActionResult GetProductos()
+        {
+            return this.Ok(this.repositorioProductos.GetAll());
+        }
+    }
+
+}
